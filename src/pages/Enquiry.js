972a1e342ba3dcc029b79f5546a51c3d7cb100e5
Qasim958 <89703982+Qasim958@users.noreply.github.com>
2022-06-09 21:05:17 +0500

    Update Enquiry.js

--- src/pages/Enquiry.js
+++ src/pages/Enquiry.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EnqForm from "../components/enqForm";
-import Estresp from "../server/establishments.json";
 import { useLocation } from "react-router";
+import { getApiData } from "../utils/fetchApiData";
+import { GET_ESTABLISHMENT_RECORDS } from "../api";
 
 const Enquiry = (props) => {
   const location = useLocation();
   const filterPara = location.state;
 
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    async function getData() {
+      const response = await getApiData(GET_ESTABLISHMENT_RECORDS);
+      if (response) {
+        setData(response);
+      }
+    }
+    getData();
+  });
   return (
     <>
       <div className="enqCont">
         <div className="row">
           {filterPara !== null ? (
-            Estresp.map((data, i) => {
+            data.map((data, i) => {
               if (filterPara === data.id) {
                 return <EnqForm key={i} name={data.establishmentName} />;
               }
